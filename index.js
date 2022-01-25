a5da7a5f1bd5d908c29e93a8e9fb92ad3836aba1
karthik <karthikhallad@gmail.com>
2022-01-25 20:30:24 +0530

    Version 0.0.3

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const PORT =3000;
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
+const engine = require('ejs-mate');
+
 
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+app.engine('ejs', engine);
 
 main().catch(err => console.log(err));
 
@@ -23,15 +26,36 @@ async function main() {
 
 const Campground = require('./models/campgrounds');
 
-app.get('/campgrounds', async (req, res) =>{
+app.get('/campgrounds', async (req, res, next) =>{
   // need to await as fetching takes a lot of time
   let Campgrounds = await Campground.find();
-  console.log(Campgrounds)
   res.render('campgrounds/index.ejs',{Campgrounds})
 })
 
+
+
+
 app.get('/campgrounds/new',async (req, res)=>{
   res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id/edit',async (req, res) =>{
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit',{campground})
+})
+
+app.delete('/campgrounds/:id',async (req, res) =>{
+  const id = req.params.id;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds')
+})
+
+app.patch('/campgrounds/:id',async (req, res) =>{
+  let updated = req.body.Campground;
+  console.log(updated)
+  await Campground.findByIdAndUpdate({_id: req.params.id},{...updated});
+  res.redirect(`/campgrounds/${req.params.id}`)
+  
 })
 
 app.get('/campgrounds/:id', async (req, res) =>{
@@ -40,8 +64,15 @@ app.get('/campgrounds/:id', async (req, res) =>{
   res.render('campgrounds/show.ejs',{campground})
 })
 
-app.get('*',()=>{
-  console.log('I DONT KNOW THIS PATH');
+app.post('/campgrounds',async (req, res) =>{
+  const body = req.body.Campground;
+  const campground = new Campground(body);
+  const {_id}=await campground.save(body).then((d) => d);
+  res.redirect(`/campgrounds/${_id}`)
+})
+
+app.use((req,res, next)=>{
+  res.status(404).send('<h1>NOT FOUND<h1><br><h2>Please check the url<h2>');
 })
 
 app.listen(PORT,()=>{
